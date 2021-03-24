b591bcb36210df9ea067ddb82551726a27afbfeb
Kunal Soni <53283003+kunal9922@users.noreply.github.com>
2021-03-24 18:45:49 +0530

    read image successfully Done

--- ChaoticCrypto.py
+++ ChaoticCrypto.py
@@ -5,7 +5,16 @@ import numpy as np
 
 class ChaoticCrypto:
     ''' encrypt and decrypt image by using Logistic Map(pseudo Random)'''
+    def ecrypt_img(self, path: str):
+        # reading an image
+        img = mpimg.imread(path)
+
+        plt.imshow(img)
+        plt.show()
 
 
 if __name__ == "__main__":
     print("hello")
+
+    imgSecure = ChaoticCrypto()
+    imgSecure.ecrypt_img('steve-jobs.jpg')
