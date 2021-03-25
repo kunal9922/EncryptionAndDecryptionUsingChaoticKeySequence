75bbebcf04771ea35564e108750810c07e42692e
Kunal Soni <53283003+kunal9922@users.noreply.github.com>
2021-03-25 16:41:25 +0530

    find out no. of pixel of an image for no. of key generation

--- ChaoticCrypto.py
+++ ChaoticCrypto.py
@@ -5,12 +5,19 @@ import numpy as np
 
 class ChaoticCrypto:
     ''' encrypt and decrypt image by using Logistic Map(pseudo Random)'''
+
     def ecrypt_img(self, path: str):
         # reading an image
         img = mpimg.imread(path)
 
         plt.imshow(img)
         plt.show()
+
+        # take the image width and height
+        height = img.shape[0]
+        width = img.shape[1]
+
+        noOfPixels = width * height
 
 
 if __name__ == "__main__":
