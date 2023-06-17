7ed4617e5879a81e0f91d05d2b5cdbbd05551382
Kunal Soni <kunalsoni9200@gmail.com>
2023-06-17 15:13:27 +0530

    blue print of ChaoticCrypto with their essential methods

--- app.py
+++ app.py
@@ -7,6 +7,20 @@ import base64
 #flask app
 app = Flask(__name__)
 
+class ChaoticCrypto:
+    ''' encrypt and decrypt image by using Chaos Logistic Map(pseudo Random)'''
+
+    def encrypt_img(self, initCond, controlPara, img):
+        ''' encrypt an image by using Chaostic key Sequence and keys complexity is based on \n
+        your passing initialCondition and controlParameter '''
+        pass
+
+    def decrypt_img(self, initCond, controlPara, enimg):
+        ''' decryption an image by using Chaostic key Sequence and keys complexity is based on \n
+        your passing initialCondition and controlParameter Note what aguments were passed to encryption is always same for decryption \n
+        other wise decryption never will possible '''
+        pass
+
 #main 
 if __name__ == "__main__":
     pass 
