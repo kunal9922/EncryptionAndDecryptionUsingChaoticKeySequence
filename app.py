647fa403ce2337243b07996e584d3f78776fe31e
Kunal Soni <kunalsoni9200@gmail.com>
2023-06-17 15:06:10 +0530

    created flask app with main

--- app.py
+++ app.py
@@ -3,3 +3,10 @@ from chaoticKeyGen import KeyGen
 import cv2
 from flask import Flask, request, jsonify, render_template
 import base64
+
+#flask app
+app = Flask(__name__)
+
+#main 
+if __name__ == "__main__":
+    pass 
